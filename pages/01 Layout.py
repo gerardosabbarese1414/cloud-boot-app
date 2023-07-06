28090f0a63b754beba38410b88a18df918d3fd53
Gerardo Sabbarese <137396058+gerardosabbarese1414@users.noreply.github.com>
2023-07-06 18:49:12 +0200

    Update 01 Layout.py

--- pages/01 Layout.py
+++ pages/01 Layout.py
@@ -1,1 +1,26 @@
 import streamlit as st
+
+
+def scegli_materiale():
+    st.subheader("Scegli il materiale del gioiello:")
+    materiali = ["Oro", "Argento", "Platino"]
+    scelta_materiale = st.selectbox("Seleziona il materiale", materiali)
+    return scelta_materiale.lower()
+
+
+def scegli_gemme():
+    gemme_disponibili = ["Diamante", "Rubino", "Smeraldo", "Zaffiro"]
+
+    st.subheader("Scegli le gemme da inserire nel gioiello:")
+    gemme_selezionate = st.multiselect("Seleziona le gemme", gemme_disponibili)
+    return [gemma.lower() for gemma in gemme_selezionate]
+
+
+def crea_gioiello():
+    materiale = scegli_materiale()
+    gemme = scegli_gemme()
+
+    st.success("Il tuo gioiello personalizzato è pronto!")
+    st.write("Materiale:", materiale)
+    st.write("Gemme:", ", ".join(gemme))
+
